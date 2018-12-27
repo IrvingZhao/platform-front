4247fac43d443652d9c8b226a7eed013a68a0049
zhao jianan <zhaojn1@lenovo.com>
2018-12-27 18:17:33 +0800

    修改 表格调用逻辑及传入参数

--- src/modules/demo/pages/list.vue
+++ src/modules/demo/pages/list.vue
@@ -13,60 +13,38 @@
             <div class="right-toolbar">
                 <el-button size="small">按钮3</el-button>
                 <el-button size="small">按钮4</el-button>
-                <el-button size="small" icon="el-icon-my-user-define" @click="diaVisible.columnDefine=true">自定义列
+                <el-button size="small" icon="el-icon-my-user-define" @click="customColumn">自定义列
                 </el-button>
             </div>
         </div>
-        <grid-js>
+        <grid-js ref="grid" :table-params="tableParams" :all-config-columns="allTableColumns"
+                 :visible-columns.sync="visibleColumns" @updateColumns="updateColumns">
             <el-table-column type="selection" width="55"></el-table-column>
-            <el-table-column slot="date" fixed prop="date" label="日期" min-width="250"></el-table-column>
+            <el-table-column fixed prop="date" label="日期" min-width="250"></el-table-column>
             <el-table-column slot="name" prop="name" label="姓名" min-width="200">
                 <template slot-scope="scope">
                     {{scope.row.name}}
                 </template>
             </el-table-column>
-            <el-table-column slot="province" prop="province" label="省份" min-width="200"></el-table-column>
+            <el-table-column slot="province" prop="province" label="省份"
+                             min-width="200"></el-table-column>
             <el-table-column slot="city" prop="city" label="市区" min-width="200"></el-table-column>
             <el-table-column slot="address" prop="address" label="地址" min-width="350">
             </el-table-column>
             <el-table-column slot="zip" prop="zip" label="邮编" min-width="200"></el-table-column>
-            <el-table-column slot="name1" prop="name" label="姓名" min-width="200"></el-table-column>
-            <el-table-column slot="province1" prop="province" label="省份" min-width="200"></el-table-column>
-            <el-table-column slot="city1" prop="city" label="市区" min-width="200"></el-table-column>
-            <el-table-column slot="address1" prop="address" label="地址" min-width="350"
+            <el-table-column slot="name1" prop="name" label="姓名1" min-width="200"></el-table-column>
+            <el-table-column slot="province1" prop="province" label="省份1"
+                             min-width="200"></el-table-column>
+            <el-table-column slot="city1" prop="city" label="市区1" min-width="200"></el-table-column>
+            <el-table-column slot="address1" prop="address" label="地址1" min-width="350"
                              :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column slot="zip1" prop="zip" label="邮编" min-width="200"></el-table-column>
-            <el-table-column slot="operator" fixed="right" label="操作" min-width="150">
+            <el-table-column key="zip1" slot="zip1" prop="zip" label="邮编1" min-width="200"></el-table-column>
+            <el-table-column fixed="right" label="操作" min-width="150">
                 <template slot-scope="scope">
                     <el-button>测试</el-button>
                 </template>
             </el-table-column>
         </grid-js>
-        <!--<div class="grid" ref="gridArea">-->
-        <!--<el-table ref="table" :data="tableData3" :height="tableHeight" size="small">-->
-        <!--<el-table-column fixed prop="date" label="日期" min-width="250"></el-table-column>-->
-        <!--<el-table-column prop="name" label="姓名" min-width="200">-->
-        <!--<template slot-scope="scope">-->
-        <!--{{scope.row.name}}-->
-        <!--</template>-->
-        <!--</el-table-column>-->
-        <!--<el-table-column prop="province" label="省份" min-width="200"></el-table-column>-->
-        <!--<el-table-column prop="city" label="市区" min-width="200"></el-table-column>-->
-        <!--<el-table-column prop="address" label="地址" min-width="350">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column prop="zip" label="邮编" min-width="200"></el-table-column>-->
-        <!--&lt;!&ndash;<el-table-column prop="name" label="姓名" min-width="200"></el-table-column>&ndash;&gt;-->
-        <!--&lt;!&ndash;<el-table-column prop="province" label="省份" min-width="200"></el-table-column>&ndash;&gt;-->
-        <!--&lt;!&ndash;<el-table-column prop="city" label="市区" min-width="200"></el-table-column>&ndash;&gt;-->
-        <!--&lt;!&ndash;<el-table-column prop="address" label="地址" min-width="350" :show-overflow-tooltip="true"></el-table-column>&ndash;&gt;-->
-        <!--&lt;!&ndash;<el-table-column prop="zip" label="邮编" min-width="200"></el-table-column>&ndash;&gt;-->
-        <!--<el-table-column fixed="right" label="操作" min-width="150">-->
-        <!--<template slot-scope="scope">-->
-        <!--<el-button>测试</el-button>-->
-        <!--</template>-->
-        <!--</el-table-column>-->
-        <!--</el-table>-->
-        <!--</div>-->
         <div class="pagination">
             <el-pagination
                     :page-sizes="[100, 200, 300, 400]"
@@ -75,8 +53,6 @@
                     :total="400">
             </el-pagination>
         </div>
-
-        <column-define :visible.sync="diaVisible.columnDefine"></column-define>
 
     </div>
 </template>
@@ -87,7 +63,7 @@
         let result = [];
         for (let i = 1; i < 20; i++) {
             result.push({
-                date: '2016-05-03',
+                date: '2016-05-' + i,
                 name: '王小虎' + i,
                 province: '上海',
                 city: '普陀区',
@@ -117,32 +93,25 @@
         return result;
     }
 
-    // import GeminiScroll from 'gemini-scrollbar';
-    import GeminiScroll from '@/plugins/geminiScrollbar';
-
-    import ColumnDefine from './columnDefine';
-
     import SearchForm from '../../../component/page/searchForm';
 
     import GridJs from '../../../component/page/grid/grid';
 
     export default {
         name: "index",
-        components: {ColumnDefine, SearchForm, GridJs},
+        components: {SearchForm, GridJs},
         data() {
             return {
                 searchForm: getSearchFormData(),
                 searchFormSmallLabels: generateSmallData(),
                 smallData: [],
-                tooltip: "",
-                diaVisible: {
-                    columnDefine: false,
+                tableParams: {
+                    data: generateTableData(),
+                    size: "small",
+                    height: "auto"
                 },
-                tableData3: generateTableData(),
-                tableHeight: "auto",
-                holdObj: {
-                    scroll: null
-                }
+                allTableColumns: [],
+                visibleColumns: [],
             }
         },
         created() {
@@ -152,46 +121,29 @@
                 {name: "测试"},
                 {name: "首页", path: "/page"}
             ]);
-            // setTimeout(() => {
-            //     this.tableHeight = this.$refs.gridArea.offsetHeight;
-            //
-            //     let div = document.createElement("div");
-            //     div.innerHTML = '<div class="gm-scrollbar -vertical"><div class="thumb"></div></div><div class="gm-scrollbar -horizontal"><div class="thumb"></div></div>';
-            //
-            //     this.$refs.table.$refs.bodyWrapper.classList.add("gm-scroll-view");
-            //     for (let i = div.children.length - 1; i >= 0; i--) {
-            //         this.$refs.table.$el.appendChild(div.children.item(i));
-            //     }
-            //
-            //     let scroll = this.holdObj.scroll = new GeminiScroll({
-            //         element: this.$refs.table.$el,
-            //         createElements: false,
-            //         offsetY: 48
-            //     });
-            //     setTimeout(() => {
-            //         scroll.create();
-            //         let leftWrapper = this.$refs.table.$refs.fixedWrapper;
-            //         if (leftWrapper) {
-            //             leftWrapper.style.height = (leftWrapper.offsetHeight + scroll.SCROLLBAR_WIDTH - 6) + "px";
-            //             let leftBodyWrapper = leftWrapper.querySelector(".el-table__fixed-body-wrapper");
-            //             if (leftBodyWrapper) {
-            //                 leftBodyWrapper.style.height = (leftBodyWrapper.offsetHeight + scroll.SCROLLBAR_WIDTH - 6) + "px";
-            //             }
-            //         }
-            //         let rightWrapper = this.$refs.table.$refs.rightFixedWrapper;
-            //         if (rightWrapper) {
-            //             rightWrapper.style.height = (rightWrapper.offsetHeight + scroll.SCROLLBAR_WIDTH - 6) + "px";
-            //             rightWrapper.style.right = "6px";
-            //             let rightBodyWrapper = rightWrapper.querySelector(".el-table__fixed-body-wrapper");
-            //             if (rightBodyWrapper) {
-            //                 rightBodyWrapper.style.height = (rightBodyWrapper.offsetHeight + scroll.SCROLLBAR_WIDTH - 6) + "px";
-            //             }
-            //         }
-            //     }, 0);
-            //
-            // }, 0);
+            this.allTableColumns = [
+                {key: "name", value: "姓名"},
+                {key: "province", value: "省份"},
+                {key: "city", value: "时区"},
+                {key: "address", value: "地址"},
+                {key: "zip", value: "邮编"},
+                {key: "name1", value: "姓名1"},
+                {key: "province1", value: "省份1"},
+                {key: "city1", value: "时区1"},
+                {key: "address1", value: "地址1"},
+                {key: "zip1", value: "邮编1"},
+            ];
+            this.visibleColumns = [
+                // "province"
+            ];
         },
         methods: {
+            updateColumns(val) {
+                this.visibleColumns = val;
+            },
+            customColumn() {
+                this.$refs.grid.editColumn();
+            },
             clearItem(key) {
                 this.searchForm[key] = "";
                 this.smallData = this.smallData.filter((item) => {
@@ -219,17 +171,6 @@
                         return [0, 0];
                     }
                 }
-            },
-            toolTipShow(e) {
-                let current = e.target;
-                // if (current.scrollWidth > current.offsetWidth) {
-                let prefix = current.getAttribute("prefix");
-                let target = current.getAttribute("target");
-                this.tooltip = prefix + target;
-                // }
-            },
-            toolTipHide() {
-                this.tooltip = "";
             },
             format(a) {
                 return a;
