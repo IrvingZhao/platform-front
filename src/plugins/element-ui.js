d8b4f973d0525a6ec4d1c0db0747fe1133144b40
zhao jianan <zhaojn1@lenovo.com>
2018-12-28 15:22:19 +0800

    添加自定义组件引入

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-
 import 'element-ui/packages/theme-chalk/src/index.scss';
 
+import XlbComponent from '../component';
+
 Vue.use(ElementUI);
+Vue.use(XlbComponent);
