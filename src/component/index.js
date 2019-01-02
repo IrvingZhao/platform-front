91cc7fa400f26203a29c6f802f90260f6feb457b
zhao jianan <zhaojn1@lenovo.com>
2019-01-02 18:26:00 +0800

    引入新增组件

--- src/component/index.js
+++ src/component/index.js
@@ -2,11 +2,14 @@ import BasePage from './basePage';
 import ConfigColumnGrid from './configColumnGrid';
 import ConfigSlot from "./configSlot";
 import ScrollArea from './scrollArea';
+import ScrollItem from './scrollItem';
 import SearchForm from './searchForm';
 import Toolbar from './toolbar';
+import TreeTable from './treeTable';
+import TreeTableColumn from './treeTableColumn';
 
 let components = [
-    BasePage, ConfigColumnGrid, ConfigSlot, ScrollArea, SearchForm, Toolbar
+    BasePage, ConfigColumnGrid, ConfigSlot, ScrollArea, ScrollItem, SearchForm, Toolbar, TreeTable, TreeTableColumn
 ];
 
 export default {
